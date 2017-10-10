7cee58c4a4aa4b05b2364204dbed557996dd9b1f
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-10 16:01:31 -0500

    exceptions handled

---
+++ main.rb
@@ -0,0 +1,44 @@
+require_relative 'patient'
+
+new_patient = Patient.new("Molly", "Weasley")
+puts new_patient.first_name
+puts new_patient.last_name
+
+def patient_contact_info
+#Error 1: NameError:  undefined local variable or method `patient_contact_info' for main:Object
+    begin
+    
+    raise NameError, "There ain't a method named patient_contact_info. Try again."
+
+    rescue NameError => e
+    print "Exception message: #{e.message}"
+    end
+end
+
+# def patient_currently_admitted?
+#     begin
+#         raise ArgumentError, "Please input proper time" 
+
+#         rescue ArgumentError => e
+#         puts "Exception message: #{e.message}"
+#         else
+#         puts "Patient currently in hospital, admitted at #{admit_time}"
+#     end
+# end
+
+puts patient_contact_info
+
+#Error 2: `patient_status': Argument is not a string (ArgumentError)
+puts new_patient.patient_status(:coma)
+
+#Error 3: `patient_currently_admitted?': wrong number of arguments (given 0, expected 1) (ArgumentError)
+# puts new_patient.patient_currently_admitted?
+
+#Error 4:  divided by 0 (ZeroDivisionError)
+print new_patient.patient_new_total(125)
+
+#Error 5: `add_med_to_list': 123.12 is not a integer (ArgumentError)
+new_patient.add_med_to_list("lipitor", 123.12)
+
+#Error 5: `patient_medications_list': Medications list is not valid. (RuntimeError)
+new_patient.patient_medications_list
---
+++ patient.rb
@@ -0,0 +1,89 @@
+class Patient
+    attr_accessor :first_name, :last_name, :status, :current_balance, :med_list
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @med_list = []
+  end
+
+  def patient_status(status)
+
+    begin
+        raise ArgumentError,"Argument is not a string" unless status.is_a? String
+
+        rescue ArgumentError => e
+        puts "Exception message: #{e.message}"
+    end
+    @status = status
+  end
+
+  def patient_currently_admitted?(admit_time)
+
+    begin
+        raise ArgumentError, "Please input proper time" unless patient_currently_admitted?
+
+        rescue ArgumentError => e
+        puts "Exception message: #{e.message}"
+        else
+        puts "Patient currently in hospital, admitted at #{admit_time}"
+    end
+  end
+
+  def patient_new_total(amount)
+    @current_balance = 0
+    puts "Current balance before calculation: #{current_balance}"
+
+    begin
+        raise ZeroDivisionError, "Not divisible by Zero (0). Check current Balance." unless @current_balance != 0
+
+    rescue ZeroDivisionError => e 
+        puts "Exception message: #{e.message}"
+    else
+        @current_balance = (current_balance + amount)/current_balance
+            # This runs only if there are no exceptions
+        @current_balance = amount
+    end
+  end
+
+  def add_med_to_list(med, dosage)
+    begin
+        raise ArgumentError,"#{dosage} is not an integer" unless dosage.is_a? Integer
+
+    rescue ArgumentError => e
+        puts "Exception message: #{e.message}"
+    else
+        puts "Medication added: #{med}"
+        @med_list.push([med,dosage])
+    end
+
+    #Create a new class called MedTypeError that inherits from StandardError and use that as seen below:
+    begin
+        raise MedTypeError, "This is not a valid medication" unless med.is_a? String
+
+        rescue MedTypeError => e
+            puts "Exception message: #{e.message}"
+        else
+            puts @med_list
+    end
+  end
+
+  def patient_medications_list
+    begin
+    raise RuntimeError, "Medications list is not valid."
+
+    rescue RuntimeError => e 
+        puts "Exception message: #{e.message}"
+    else
+    puts "User medication list:"
+    puts @med_list
+    ensure
+     # This needs to run whether there was an exception or not.
+     puts "Disclaimer: The hospital is not responsible for the user's health due to medications they may be using."
+    end
+   
+  end
+end
+
+class MedTypeError < StandardError
+end
